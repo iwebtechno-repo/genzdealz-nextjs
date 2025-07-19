73b7e7e2d2b0609b9badb66cd020cd8c2c1349b8
Kushal Trivedi <40542375+kushaltrivedi5@users.noreply.github.com>
2025-07-18 22:49:08 -0400

    Remove oldcode folder from repository

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,21 +66,10 @@ const config: Config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        ripple: {
-          "0%": {
-            transform: "scale(0)",
-            opacity: "0.5",
-          },
-          "100%": {
-            transform: "scale(4)",
-            opacity: "0",
-          },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        ripple: "ripple 0.6s linear",
       },
     },
   },
